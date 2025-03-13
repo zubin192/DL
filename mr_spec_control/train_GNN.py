5cd6c4327a2a1fd1b512d47f9e5d4e171d0529be
natbut <nlbutler18@gmail.com>
2025-03-12 18:04:26 -0700

    train GNN config

--- mr_spec_control/train_GNN.py
+++ mr_spec_control/train_GNN.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     vmas_device = "cuda" # @param {"type":"string"}
 
     # Load task configuration
-    task_config_path = "mr_spec_control/conf/task/custom_vmas/discovery_mothership.yaml"
+    task_config_path = "mr_spec_control/conf/task/custom_vmas/discovery_mothership_GNN.yaml"
     task = CustomVmasTask.DISCOVERY_OBSTACLES.get_from_yaml(task_config_path)
 
     # Load RL algorithm config
@@ -63,8 +63,8 @@ if __name__ == "__main__":
 
     experiment_config.max_n_frames = 1_000_000 # Number of frames before training ends
     experiment_config.gamma = 0.99
-    experiment_config.on_policy_collected_frames_per_batch = 3_000 # Number of frames collected each iteration (max_steps from config * n_envs_per_worker)
-    experiment_config.on_policy_n_envs_per_worker = 100 # Number of vmas vectorized enviornemnts (each will collect up to max_steps steps, see max_steps in task_config -> 50 * max_steps = 5_000 the number above)
+    experiment_config.on_policy_collected_frames_per_batch = 1_000 # Number of frames collected each iteration (max_steps from config * n_envs_per_worker)
+    experiment_config.on_policy_n_envs_per_worker = 3 # Number of vmas vectorized enviornemnts (each will collect up to max_steps steps, see max_steps in task_config -> 50 * max_steps = 5_000 the number above)
     # experiment_config.on_policy_n_minibatch_iters = 32
     # experiment_config.on_policy_minibatch_size = 64
 
@@ -73,7 +73,7 @@ if __name__ == "__main__":
     experiment_config.share_policy_params = True # Policy parameter sharing
     experiment_config.evaluation_interval = 5*experiment_config.on_policy_collected_frames_per_batch
     # experiment_config.evaluation_interval = 12_000 # Interval in terms of frames, will evaluate every eval_interval/frames_per_batch = 5 iterations
-    experiment_config.evaluation_episodes = 100 # Number of vmas vectorized enviornemnts used in evaluation
+    experiment_config.evaluation_episodes = 3 # Number of vmas vectorized enviornemnts used in evaluation
 
     experiment_config.loggers = ["csv"] # Log to csv, usually you should use wandb
 
@@ -88,24 +88,26 @@ if __name__ == "__main__":
 
     experiment = Experiment(
                 algorithm_config=algorithm_config,
-                # model_config=GnnConfig(
-                #     topology="from_pos",
-                #     self_loops=False,
-                #     gnn_class=torch_geometric.nn.conv.GATv2Conv,
-                #     edge_radius=0.5,
-                #     gnn_kwargs={},
-                # ),
-                model_config=SequenceModelConfig(
+                model_config=GnnConfig(
+                    topology="from_pos",
+                    position_key="position_key",
+                    exclude_pos_from_node_features=False,
+                    pos_features=2,
+                    edge_radius=1.0,
+                    self_loops=False,
+                    gnn_class=torch_geometric.nn.conv.GATv2Conv,
+                    gnn_kwargs={},
+                ),
+                critic_model_config=SequenceModelConfig(
                     model_configs=[
                         # MlpConfig(num_cells=[16], activation_class=nn.Tanh, layer_class=nn.Linear),
                         GnnConfig(
-                            topology="from_pos",
+                            topology="full",
                             position_key=None,
-                            edge_radius=1.0,
                             self_loops=False,
                             gnn_class=torch_geometric.nn.conv.GraphConv,
                         ),
-                        MlpConfig(num_cells=[6], activation_class=nn.Tanh, layer_class=nn.Linear),
+                        MlpConfig(num_cells=[4], activation_class=nn.Tanh, layer_class=nn.Linear),
                     ],
                     intermediate_sizes=[8], # TODO investigate this (was [5,3] for NAVIGATION scenario)
                 ),
